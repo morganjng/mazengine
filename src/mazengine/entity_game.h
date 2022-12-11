ceb50f32f525615cd1e3292414567f319be68e8d
Morgan Gribbins <morganjng@gmail.com>
2022-12-11 18:56:12 -0500

    heheheh

---
+++ src/mazengine/entity_game.h
@@ -0,0 +1,14 @@
+#ifndef ENTITY_GAME_H_
+#define ENTITY_GAME_H_
+
+#include "mazengine.h"
+
+namespace mazengine {
+
+	class EntityGame : public Game {};
+
+	class
+
+}; // namespace mazengine
+
+#endif // ENTITY_GAME_H_
