4a9aa2db7b6699283ee475486053c6606e42ef09
Morgan Gribbins <morganjng@gmail.com>
2023-01-22 22:12:18 -0500

    getting rid of old and bad tilestuff

--- src/tile_map.cc
+++
@@ -1,7 +0,0 @@
-#include "mazengine/tile_map.h"
-
-using namespace mazengine;
-
-std::vector<TileLayer> *TileMap::get_layers() { return &layers; }
-
-std::vector<TileEntity> *TileMap::get_objects() { return &objects; }
