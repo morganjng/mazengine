b95be2c2a5bc8dd8bbbe2891351ce53f43a038c0
Morgan Gribbins <morganjng@gmail.com>
2023-01-25 19:35:09 -0500

    ougd!

--- src/geometry.cc
+++ src/geometry.cc
@@ -1,3 +1,9 @@
 #include <geometry.h>
 
-namespace mazengine {}
+namespace mazengine {
+	bool Rect::Contains(int x, int y) {
+		return (this->x <= x && this->x + this->w >= x && this->y <= y &&
+				this->y + this->h >= y);
+	}
+
+} // namespace mazengine
