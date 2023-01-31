8d53ff7269d65bd024dfa7e306259b0b4527387f
Morgan Gribbins <morganjng@gmail.com>
2023-01-31 15:39:52 -0500

    idk....

--- src/maz_rb.cc
+++ src/maz_rb.cc
@@ -5,4 +5,15 @@
 
 using namespace Rice;
 
-extern "C" void Init_libmaz_rb() {}
+Object get_game() {
+	if (mazengine::Engine::engine) {
+		return String(mazengine::Engine::engine->GetGame()->name);
+	} else {
+		return String("Sorry! There isn't any engine running");
+	}
+}
+
+extern "C" void Init_libmaz_rb() {
+	Module rb_mMaz =
+		define_module("Maz").define_module_function("GetGame", &get_game);
+}
